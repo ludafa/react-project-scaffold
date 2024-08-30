67d9251a90e66eb063afde18a5035aea35989e59
jinzhubaofu <leonlu@outlook.com>
2024-08-30 08:25:09 +0800

    add: typo and upgrade dep version

--- uno.config.ts
+++ uno.config.ts
@@ -3,9 +3,9 @@ import presetUno from '@unocss/preset-uno';
 import transformerDirectives from '@unocss/transformer-directives';
 import transformerVariantGroup from '@unocss/transformer-variant-group';
 import { animatedUno } from 'animated-unocss';
-import { defineConfig, UserConfig } from 'unocss';
+import { defineConfig, UserConfig, presetTypography } from 'unocss';
 
 export default defineConfig({
-  presets: [presetUno(), presetIcons(), animatedUno()],
+  presets: [presetUno(), presetIcons(), animatedUno(), presetTypography()],
   transformers: [transformerVariantGroup(), transformerDirectives()],
 }) as UserConfig;
